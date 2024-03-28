2fa7ddab0e1000e59be7a3b1021cdaf09cd5706f
Steven Kissack <steven@osar.im>
2024-03-29 00:12:34 +0700

    mvp sales report page

--- application/views/pages/sales_report.php
+++ application/views/pages/sales_report.php
@@ -1,15 +1,80 @@
-<?php extend('layouts/backend_layout'); ?>
+<?php extend('layouts/backend_layout'); 
+
+require_once __DIR__ . '/../../../vendor/koolreport/core/autoload.php';
+use \koolreport\widgets\koolphp\Table;
+use \koolreport\widgets\google\BarChart;
+
+?>
 
 <?php section('content'); ?>
 
 <div class="container-fluid backend-page" id="sales-report-page">
 
-    <div class="row" id="sales-report">
-        <div class="col-12 col-md-5">
-            
+    <div class="row">
+    <div class="col-12">
+        <div class="card mb-4">
+            <div class="card-header">
+                <h5 class="fw-light text-black-50 mb-0">
+                    3 Month Sales Counts By Service per Week
+                </h5>
+            </div>
+            <div class="card-body">
+                <?php 
+                BarChart::create([
+                    "dataSource" => vars('chart_data'),
+                    "options" => [
+                        "isStacked" => true,
+                        "hAxis" => ["title" => "Sales Count"],
+                        "vAxis" => ["title" => "Period"],
+                    ]
+                ]);
+                ?>
+            </div>
         </div>
     </div>
+</div>
 
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h5 class="fw-light text-black-50 mb-0">
+                        Last 90 Days Sales
+                    </h5>
+                </div>
+                <div class="card-body">
+                <?php 
+                    Table::create(array(
+                        "dataSource"=>vars('sales_history'),
+                        "columns"=>array(
+                            "id"=>array(
+                                "label"=>"ID"
+                            ),
+                            "start_datetime"=>array(
+                                "type"=>"date",
+                                "label"=>"Sale Date",
+                            ),
+                            "price"=>array(
+                                'formatValue'=>function($value, $row, $cKey)
+                                                {
+                                                    if ($cKey === 'price')
+                                                        return number_format($value)." ".$row["currency"];
+                                                    else
+                                                        return $value;
+                                                },
+                                "label"=>"Price"
+                            ),
+                            "service_name"=>array(
+                                "label"=>"Service"
+                            ),
+                        ),
+                    ));
+                ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php end_section('content'); ?>
